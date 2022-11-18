6442a8c71affe0f010104784df5f837a1a72f725
akshatpathak26 <akshatpathak08@gmail.com>
2022-11-18 20:14:55 +0530

    4th commit

--- promises.js
+++ promises.js
@@ -17,3 +17,26 @@ const promise = new Promise((resolve,reject)=>{
 console.log(promise);
 
 promise.then((data)=> console.log(data)).catch((err)=> console.log(err))
+
+
+// const btn = document.querySelector('.btn');
+
+// btn.addEventListener('click',()=>{
+// addColor(1000,'.first','red').then(()=>addColor(3000,'.second','blue'))
+// .then(()=>addColor(2000,'.third','green'))
+// .catch((err)=> console.log(err))
+// })
+
+// function addColor(time, selector, color) {
+//     const element = document.querySelector(selector)
+//     return new Promise((resolve, reject)=>{
+//         if (element) {
+//             setTimeout(() => {
+//                 element.style.color = color
+//                 resolve()
+//             }, time);
+//         } else{
+//             reject(`there is no such element : "${selector}"`)
+//         }
+//     })
+// }
